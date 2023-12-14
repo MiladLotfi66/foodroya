cfc82cee55c423c800ed72990b853d20e43eb8f4
miladlotfi <miladlotfi66@gmail.com>
2023-12-14 11:37:55 +0330

    add normal shadow in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{html,js}",
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors:{
-        'brown':{
-100:'#ECE0D1',
-300:"#DBC1AC",
-600:"#967259",
-900:"#634832",
-        }
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        normal: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
       },
     },
   },
   plugins: [],
-}
+};
